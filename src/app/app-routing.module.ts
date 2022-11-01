77c49de84f7dc158eca34748f0c88707c8e3fc0e
Allison Claramunt Gonzalez <1567514@uab.cat>
2022-11-01 20:01:17 +0100

    Save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', component:HeroesComponent},
+  {path:'home', component:HomeComponent},
+  {path:'heroes', component: HeroesComponent},
+  {path:'heroe/:id', component: HeroeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'**', component:HeroesComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeaderComponent,
+    HeroeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
